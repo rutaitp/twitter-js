ea9fac85dfd5254806aad8a6278b7bfdbf2714bc
rutaitp <rk2906@nyu.edu>
2018-03-05 18:00:29 -0500

    routes

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 //main application
+ const express = require("express");
+ const app = express();
+
+//log any URI
+app.use(function(req, res, next){
+  console.log("Page is loading!");
+  // res.send("Page is loading!");
+  next();
+  // console.log(req.params);
+});
+
+app.get("/", function(req, res){
+  res.send("Welcome!");
+});
+
+app.get("/news", function(req, res){
+  res.send("News suck!");
+});
+
+
+//listen to a port
+const port = 3000;
+
+app.listen(port, function() {
+  console.log(`Server running on port ${3000}!`);
+});
